c0ad2fc9fb86bb67f44033f1d658f860008fac0a
Tanishka444 <tanishkalonkar1234@gmail.com>
2026-05-08 11:23:50 +0530

    Final changes deployment

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://politicians-portal1.vercel.app/', credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
